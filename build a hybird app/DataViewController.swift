2824e2e8ebeb62fe120febe18f8f06a912e9c3ae
Damien Bell <damiensbell@gmail.com>
2014-10-19 20:00:03 -0500

    Initial Commit

---
+++ build a hybird app/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  build a hybird app
+//
+//  Created by Damien Bell on 10/19/14.
+//  Copyright (c) 2014 Damien Bell. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
---
+++ build a hybird appTests/build_a_hybird_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  build_a_hybird_appTests.swift
+//  build a hybird appTests
+//
+//  Created by Damien Bell on 10/19/14.
+//  Copyright (c) 2014 Damien Bell. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class build_a_hybird_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
